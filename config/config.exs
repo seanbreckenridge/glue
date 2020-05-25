0fb7fa369344adf5a7370741da839b35b7e9cf42
Sean Breckenridge <seanbrecke@gmail.com>
2020-05-24 23:18:44 -0700

    reduce albums interval time

--- config/config.exs
+++ config/config.exs
@@ -17,7 +17,7 @@ tmdb_api_key = System.get_env("TMDB_API_KEY")
 # at Glue.GenCache.External.ServiceKey
 # e.g. Glue.GenCache.External.Albums for "albums"
 config :glue,
-  albums: [db_id: 1, service_key: "albums", port: 8083, refresh_ms: :timer.hours(6)],
+  albums: [db_id: 1, service_key: "albums", port: 8083, refresh_ms: :timer.hours(3)],
   wca: [db_id: 2, service_key: "wca", port: 8010, refresh_ms: :timer.hours(24 * 7)],
   mal: [db_id: 3, service_key: "mal", refresh_ms: :timer.hours(6)],
   trakt: [db_id: 4, service_key: "trakt", refresh_ms: :timer.minutes(45)],
