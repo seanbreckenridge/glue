9683bef0e1480e60d09412146c83180a56d92cb5
Sean Breckenridge <seanbrecke@gmail.com>
2020-10-14 01:25:07 -0700

    add basic loading animation

--- assets/frontend/ui/computer.tsx
+++ assets/frontend/ui/computer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, Dispatch, SetStateAction } from "react";
 import {
   AppContextProvider,
   Context,
@@ -18,12 +18,59 @@ const Computer: React.FC<{}> = () => {
   );
 };
 
-// TODO: add loading animation
+const loadingText: string[] = [
+  "Loading...",
+  "---------------",
+  "sean.fish",
+  "---------------",
+  "Devices",
+  "   Unit 0 - Backend API ... DONE",
+  "   Unit 1 - Frontend Assets ... DONE",
+  "---------------",
+  "BOOTLOADED",
+];
+
+const loadingTextLength = loadingText.length;
+const frameDuration = 100;
+
+function renderFrame(
+  currentFrame: number,
+  setLoadingFunc: Dispatch<SetStateAction<number>>
+): void {
+  // there are still frames to render
+  if (currentFrame < loadingTextLength) {
+    setLoadingFunc((oldFrame) => {
+      return oldFrame + 1;
+    });
+    setTimeout(() => {
+      console.log(currentFrame);
+      renderFrame(currentFrame + 1, setLoadingFunc);
+    }, frameDuration);
+  }
+}
+
 export const HomeScreen: React.FC<{}> = () => {
+  const [loading, setLoading] = useState<number>(0);
+
+  useEffect(() => {
+    renderFrame(loading, setLoading);
+  }, []);
+
   return (
     <AppContextConsumer>
       {(value: Context) => {
-        return <GUI backgroundColor={value.backgroundColor} />;
+        if (loading >= loadingTextLength) {
+          // animation is over
+          return <GUI backgroundColor={value.backgroundColor} />;
+        } else {
+          return (
+            <div className="loading-text pixel">
+              {loadingText.map((line: string, index: number) => {
+                return <div key={index}>{loading >= index ? line : ""}</div>;
+              })}
+            </div>
+          );
+        }
       }}
     </AppContextConsumer>
   );
