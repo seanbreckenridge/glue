93fceec80e6579a0891fc729107de334329df5c0
Sean Breckenridge <seanbrecke@gmail.com>
2020-10-14 18:21:10 -0700

    remove debug print in count

--- lib/glue_web/controllers/page_hit_controller.ex
+++ lib/glue_web/controllers/page_hit_controller.ex
@@ -8,7 +8,6 @@ defmodule GlueWeb.PageHitController do
 
   def index(conn, _params) do
     page_hit_count = PageHits.page_hit_count()
-    IO.inspect(page_hit_count)
     render(conn, "count.json", page_hits: page_hit_count)
   end
 
